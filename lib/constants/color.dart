b03315b69d64c6109aab87a323b23976e5386f43
Shalwincs <shalwincs7@gmail.com>
2025-08-02 14:03:52 +0530

    final update 02-08-2025

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -8,13 +8,4 @@ class AppColors {
   static const black = Color(0xFF000000);
   static const red = Color(0xFFE74040);
   static const lightGrey = Color(0xFF585858);
-
-  // static const error = Color(0xFFFF0000);
-  // static const bordergrey = Color(0xFFA8A2A2);
-
-  // static const darkgrey = Color(0xFF5D5151);
-  // static const green = Color(0xFF2CB93A);
-  // static const lightgrey = Color(0xFFA59494);
-  // static const lightgrey2 = Color(0xFF888B8E);
-  // static const lightgrey3 = Color(0xFF797882);
 }
